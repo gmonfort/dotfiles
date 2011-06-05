a3c845fc3f888b78731200ae52743d79b152c7a7
German Monfort <german.monfort@gmail.com>
2011-06-05 02:40:06 -0300

    fixing setup script

--- setup.sh
+++ setup.sh
@@ -1,32 +1,23 @@
-#!/bin/sh
+#!/bin/bash
 
-HOME="~"
+HOME=$(readlink -fn ~)
 if [ ! -z "$1" ]; then
     HOME="$1"
 fi 
-
-DOTFILES_REPO="git://github.com/gmonfort/dotfiles.git"
-
-cd $HOME
-if [ ! -d "$HOME/code" ]; then
-    mkdir "$HOME/code"
-    cd $HOME/code
-fi
-
-git clone $DOTFILES_REPO
-cd dotfiles
+echo "Using HOME: $HOME"
 
 # make symlinks to $HOME
 # ensure we're on the base of the dotfiles repo
-toplevel="$(git rev-parse --show-toplevel)" || echo "Not in git directory" && exit
+toplevel="$(git rev-parse --show-toplevel)" || (echo "Not in git directory" && exit 1)
 
+echo "symlinking ..."
 cd "$toplevel" && for f in $(ls -A); do
-  [ $f = .git ] && continue
-  [ -f $HOME/$f || -L $HOME/$f ] && mv $HOME/"$f"{,.bak}
+  [[ $f = .git ]] && continue
+  [[ -f $HOME/$f || -L $HOME/$f ]] && mv $HOME/"$f"{,.bak}
   ln -s "$toplevel/$f" $HOME/"$f"
 done
 
 # RVM
-bash < <( curl http://rvm.beginrescueend.com/releases/rvm-install-head )
+bash < <( curl https://rvm.beginrescueend.com/install/rvm )
 
 exit 0
